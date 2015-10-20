678556010db2f93e087743202fa639db9859e7dc
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-20 21:56:46 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/Depak.pm
@@ -0,0 +1,7 @@
+package Acme::PERLANCAR::Test::Depak;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Test module
---
+++ lib/Acme/PERLANCAR/Test/Depak/Packed.pm
@@ -0,0 +1,10 @@
+package Acme::PERLANCAR::Test::Depak::Packed;
+
+# DATE
+# VERSION
+
+# PACKED_MODULES
+# PACKED_DISTS
+
+1;
+# ABSTRACT: Test module (just an empty module for metadata)
